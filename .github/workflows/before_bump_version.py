7e9540a76e3faba71702d82c27ee302cfbd3cc89
David Berroa <davbr87@gmail.com>
2021-02-04 03:04:41 -0400

    Update main.yml

--- .github/workflows/before_bump_version.py
+++ .github/workflows/before_bump_version.py
@@ -5,7 +5,7 @@ branchName = os.environ.get('SOURCE_BRANCH')
 print(f'SOURCE_BRANCH: {branchName}')
 
 branchName = branchName.replace('refs/heads/', '').replace('/', '-')
-createArtifact = 'true' if branchName == 'master' or branchName == 'ci' else 'false'
+createRelease = 'true' if branchName == 'master' or branchName == 'ci' else 'false'
 
 print(f'::set-output name=source_branch::{branchName}')
-print(f'::set-output name=create_artifact::{createArtifact}')
+print(f'::set-output name=create_release::{createRelease}')
